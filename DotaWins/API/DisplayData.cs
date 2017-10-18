4c6f2f2ab0b629fa561b250931e7781a896c2a44
John Pomeroy <jpcoded@gmail.com>
2017-10-18 17:42:58 -0400

    added averages into DisplayData for later use.

--- DotaWins/API/DisplayData.cs
+++ DotaWins/API/DisplayData.cs
@@ -30,19 +30,60 @@ namespace DotaWins
             public float AverageLastHits { get; private set; }
             public int[] WinLosses { get; set; }
 
-
+                               
             public void ConsumeRecentMatches(Match[] recentMatches)
             {
                 Clear();
                 if (recentMatches != null && recentMatches.Length > 0)
                 {
+                    float wonMatches = 0;
+                    var totalSeconds = 0;
+                    float totalKills = 0;
+                    float totalDeaths = 0;
+                    float totalAssists = 0;
+                    float totalXPM = 0;
+                    float totalGPM = 0;
+                    float totalHeroDamage = 0;
+                    float totalTowerDamage = 0;
+                    float totalHeroHealing = 0;
+                    float totalLastHits = 0;
+
                     RecentMatches = recentMatches;
                     WinLosses = new int[recentMatches.Length];
 
                     for (var i = 0; i < RecentMatches.Length; i++)
                     {
+                        totalSeconds += recentMatches[i].duration;
+
+                        totalKills += recentMatches[i].kills;
+                        totalDeaths += recentMatches[i].deaths;
+                        totalAssists += recentMatches[i].assists;
+                        totalXPM += recentMatches[i].xp_per_min;
+                        totalGPM += recentMatches[i].gold_per_min;
+                        totalHeroDamage += recentMatches[i].hero_damage.GetValueOrDefault(0);
+                        totalTowerDamage += recentMatches[i].tower_damage.GetValueOrDefault(0);
+                        totalHeroHealing += recentMatches[i].hero_healing.GetValueOrDefault(0);
+                        totalLastHits += recentMatches[i].last_hits;
                         WinLosses[i] = recentMatches[i].Won ? 1 : -1;
+                        if (recentMatches[i].Won)
+                        {
+                            wonMatches++;
+                        }
                     }
+
+                    Winrate = wonMatches / recentMatches.Length;
+                    var sumDuration = new TimeSpan(0, 0, totalSeconds);
+                    AverageDuration = new TimeSpan(sumDuration.Ticks / recentMatches.Length);
+
+                    AverageKills = totalKills / recentMatches.Length;
+                    AverageDeaths = totalDeaths / recentMatches.Length;
+                    AverageAssists = totalAssists / recentMatches.Length;
+                    AverageXPM = totalXPM / recentMatches.Length;
+                    AverageGPM = totalGPM / recentMatches.Length;
+                    AverageHeroDamage = totalHeroDamage / recentMatches.Length;
+                    AverageTowerDamage = totalTowerDamage / recentMatches.Length;
+                    AverageHeroHealing = totalHeroHealing / recentMatches.Length;
+                    AverageLastHits = totalLastHits / recentMatches.Length;
                 }
             }
 
