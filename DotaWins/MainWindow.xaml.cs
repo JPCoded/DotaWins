8d25a5e73fff734accccd385b032a1e547aefaf7
John Pomeroy <jpcoded@gmail.com>
2017-12-01 16:04:54 -0500

    Changed way string is formatted so it's formatted in xaml instead of code behind

--- DotaWins/MainWindow.xaml.cs
+++ DotaWins/MainWindow.xaml.cs
@@ -1,37 +1,36 @@
 ﻿#region
 
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
 using LiveCharts;
 using LiveCharts.Wpf;
 using OxyPlot;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
 
 #endregion
 
 namespace DotaWins
 {
-
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
-       
-
         public MainWindow()
         {
             InitializeComponent();
             DataContext = this;
         }
-      
+
         public PlayerDisplay PlayerDisplays { get; set; }
-       
-     //   public IList<DataPoint> Points { get; set; }
-    
+
+        //   public IList<DataPoint> Points { get; set; }
+
         private async void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
-
             PlayerDisplays = new PlayerDisplay();
 
             await PlayerDisplays.UpdateAsync(txtPlayerId.Text, 7);
@@ -40,47 +39,47 @@ namespace DotaWins
             gpmGraph.ItemsSource = GetPoints(PlayerDisplays.Data.GPM);
             xpmGraph.ItemsSource = GetPoints(PlayerDisplays.Data.XPM);
             //not best solution, but having a chart already made and making it equal new chart didn't work
-            grdKDA.Children.Add(CreateKda(PlayerDisplays.Data.AverageKills, PlayerDisplays.Data.AverageDeaths,PlayerDisplays.Data.AverageAssists));
-          
+            grdKDA.Children.Add(CreateKda(PlayerDisplays.Data.AverageKills, PlayerDisplays.Data.AverageDeaths,
+                PlayerDisplays.Data.AverageAssists));
 
-            lblWR_D.Content = $"{PlayerDisplays.Data.Winrate:P}";
+
+            lblWR_D.Content = PlayerDisplays.Data.Winrate;
             lblADuration_D.Content = PlayerDisplays.Data.AverageDuration;
 
-            lblAAssists_D.Content = $"{PlayerDisplays.Data.AverageAssists:F1}";
-            lblADeaths_D.Content = $"{PlayerDisplays.Data.AverageDeaths:F1}";
-            lblAKills_D.Content = $"{PlayerDisplays.Data.AverageKills:F1}";
+            lblAAssists_D.Content = PlayerDisplays.Data.AverageAssists;
+            lblADeaths_D.Content = PlayerDisplays.Data.AverageDeaths;
+            lblAKills_D.Content = PlayerDisplays.Data.AverageKills;
 
-            lblAXPM_D.Content = $"{PlayerDisplays.Data.AverageXPM:F1}";
-            lblAGPM_D.Content = $"{PlayerDisplays.Data.AverageGPM:F1}";
+            lblAXPM_D.Content = PlayerDisplays.Data.AverageXPM;
+            lblAGPM_D.Content =  PlayerDisplays.Data.AverageGPM;
 
-            lblAHeroDamage_D.Content = $"{PlayerDisplays.Data.AverageHeroDamage:F1}";
-            lblATowerDamage_D.Content = $"{PlayerDisplays.Data.AverageTowerDamage:F1}";
-            lblAHeroHealing_D.Content = $"{PlayerDisplays.Data.AverageHeroHealing:F1}";
-            lblALastHits_D.Content = $"{PlayerDisplays.Data.AverageLastHits:F1}";
+            lblAHeroDamage_D.Content = PlayerDisplays.Data.AverageHeroDamage;
+            lblATowerDamage_D.Content = PlayerDisplays.Data.AverageTowerDamage;
+            lblAHeroHealing_D.Content =PlayerDisplays.Data.AverageHeroHealing;
+            lblALastHits_D.Content = PlayerDisplays.Data.AverageLastHits;
         }
-      
+
         private static CartesianChart CreateKda(float kills, float deaths, float assists)
         {
-          var tempChart = new CartesianChart();
-            tempChart.AxisX.Add(new Axis() { Labels = new[] { "Kills", "Deaths", "Assists" } });
-           
-          tempChart.AxisY.Add( new Axis());
-         
-           var  seriesCollection = new SeriesCollection
+            var tempChart = new CartesianChart();
+            tempChart.AxisX.Add(new Axis {Labels = new[] {"Kills", "Deaths", "Assists"}});
+
+            tempChart.AxisY.Add(new Axis());
+
+            var seriesCollection = new SeriesCollection
             {
-                new ColumnSeries()
+                new ColumnSeries
                 {
-                   
-                    Values = new ChartValues<float> { kills,deaths, assists }
+                    Values = new ChartValues<float> {kills, deaths, assists}
                 }
             };
             tempChart.Series = seriesCollection;
-          return tempChart;
+            return tempChart;
         }
 
         private static IEnumerable<DataPoint> GetPoints(IEnumerable<float> pointsList)
         {
-           var points = new List<DataPoint>();
+            var points = new List<DataPoint>();
 
             var x = 0;
             foreach (var pm in pointsList)
@@ -94,17 +93,17 @@ namespace DotaWins
 
         private void UpdateWinLossGraph(IEnumerable<int> winLoseList)
         {
-            var Points = new List<DataPoint>();
+            var points = new List<DataPoint>();
 
             var x = 0;
             var currentWl = 0;
             foreach (var outcome in winLoseList)
             {
                 currentWl += outcome;
-                Points.Add(new DataPoint(x, currentWl));
+                points.Add(new DataPoint(x, currentWl));
                 x++;
             }
-            winLossGraph.ItemsSource = Points;
+            winLossGraph.ItemsSource = points;
         }
     }
 }
